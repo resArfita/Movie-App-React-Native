ed03affe56dc44f47af21b0fe12a7e0f0319d39e
res Arfita <hresaarfita@gmail.com>
2024-07-01 18:21:10 +0700

    develop homescreen layout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,27 +1,57 @@
 import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, StatusBar, ScrollView, StyleSheet } from 'react-native'
+import type { MovieListProps } from '../types/app'
+import MovieList from '../components/movies/MovieList'
 
-const Home = ({ navigation }: any): JSX.Element => {
+const movieLists: MovieListProps[] = [
+  {
+    title: 'Now Playing in Theater',
+    path: 'movie/now_playing?language=en-US&page=1',
+    coverType: 'backdrop',
+  },
+  {
+    title: 'Upcoming Movies',
+    path: 'movie/upcoming?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: 'movie/top_rated?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Popular Movies',
+    path: 'movie/popular?language=en-US&page=1',
+    coverType: 'poster',
+  },
+]
+
+
+const Home = (): JSX.Element => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.marginBottom20}>Movie Page</Text>
-      <Button
-        title="Pergi Ke MovieDetail"
-        onPress={() => navigation.navigate("MovieDetail")}
-      />
-    </View>
-  );
-};
-
-export default Home;
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: StatusBar.currentHeight ?? 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+    rowGap: 16,
   },
-  marginBottom20: {
-    marginBottom: 20,
-  },
-});
+})
+
+export default Home;
---
+++ src/screens/MovieDetail.tsx
@@ -0,0 +1,55 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { API_URL, API_ACCESS_TOKEN } from '@env'
+
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+        throw new Error('ENV not found')
+    }
+
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    };
+
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData();
+        }}
+      />
+    </View>
+  );
+};
+
+export default MovieDetail;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  marginBottom20: {
+    marginBottom: 20,
+  },
+});
